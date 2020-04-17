e8e2ce7a921482552f8a649124f2f03204dcbafe
Emerson-Vitor <63209398+Emerson-Vitor@users.noreply.github.com>
2020-04-16 22:11:59 -0300

    Site de estudos de JS, HTML e CSS

---
+++ script.js
@@ -0,0 +1,28 @@
+function data(){
+    var data = new Date()
+    var horas = data.getHours()
+    var dia = data.getDate()
+    var D1 = document.querySelector("p#D1")
+    var D2 = document.querySelector("p#D2")
+    var text = document.querySelector("p#text")
+    var diaC = 18
+    var horasC = 20
+    var resH = horasC - horas
+    var resD = diaC - dia
+    if (horas > horasC){
+        resD -= 1
+        if (resH <= 0)
+        resH += 24 
+    }
+    D1.innerText = `${resD}D`
+    D2.innerText = `${resH}H`
+    text.innerHTML = `O sorteio irar ocorrer <br/> dia ${diaC} as ${horasC}:00h `
+}
+var body = document.querySelector("body")
+var t = screen.width
+var g = screen.height
+if (t>=701){
+    body.style.backgroundSize = `100% ${t+40}px`
+}
+
+console.log(`${t} ` + g)
